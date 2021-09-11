ec75ea39e2dd0a661a0eb7229917080cfcbc3d82
Denny Hong <hong961127@gmail.com>
2021-09-11 02:04:36 -0400

    Finish slots

---
+++ section-6.2-slots/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <Form>
+    <template v-slot:help>
+      <!-- Inversion of control -->
+      <p>{{ help }}</p>
+    </template>
+
+    <template v-slot:fields>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="username" />
+      <input type="text" placeholder="password" />
+    </template>
+
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+
+    <p>Dummy text!</p>
+  </Form>
+
+  <Form>
+    <template v-slot:help>
+      <p>Contact help text.</p>
+    </template>
+
+    <template v-slot:fields>
+      <input type="text" placeholder="name" />
+      <input type="text" placeholder="message" />
+    </template>
+
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </Form>
+
+  <p>Dummy text!</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import Form from "@/components/Form.vue";
+
+export default defineComponent({
+  name: "App",
+
+  components: {
+    Form,
+  },
+
+  data() {
+    return {
+      help: "This is some help text.",
+    };
+  },
+});
+</script>
---
+++ section-6.2-slots/src/components/Form.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <div class="help">
+      <slot name="help"></slot>
+    </div>
+
+    <!-- Define default content in the slot -->
+    <div class="fields">
+      <slot name="fields">No fields to render.</slot>
+    </div>
+
+    <div class="buttons">
+      <slot name="buttons"></slot>
+    </div>
+
+    <slot></slot>
+  </form>
+</template>
